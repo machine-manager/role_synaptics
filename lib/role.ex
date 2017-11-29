cf8616dedee5dcbff5537e4503467c4d7e3b99a5
Ivan Kozik <ivan@ludios.org>
2017-11-29 02:48:38 +0000

    Create xorg.conf.d because it might not exist in a new debootstrapped Debian

--- lib/role.ex
+++ lib/role.ex
@@ -1,13 +1,17 @@
-alias Converge.Util
+alias Converge.{All, Util}
 
 defmodule RoleSynaptics do
 	require Util
-	import Util, only: [conf_file: 1]
+	import Util, only: [conf_file: 1, conf_dir: 1]
 
 	def role(_tags \\ []) do
 		%{
 			desired_packages:  ["xserver-xorg-input-synaptics"],
-			post_install_unit: conf_file("/etc/X11/xorg.conf.d/70-synaptics.conf"),
+			post_install_unit: %All{units: [
+				conf_dir("/etc/X11"),
+				conf_dir("/etc/X11/xorg.conf.d"),
+				conf_file("/etc/X11/xorg.conf.d/70-synaptics.conf"),
+			]}
 		}
 	end
 end
